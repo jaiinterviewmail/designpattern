55c04ec4b6e96074520b63ed5fdf15148a0da67b
jaiinterviewmail <jaiinterviewmail@gmail.com>
2025-08-17 07:37:30 +0530

    second commit

--- src/com/jai/bp/MyfeatureBranch.java
+++ src/com/jai/bp/MyfeatureBranch.java
@@ -2,7 +2,8 @@ package com.jai.bp;
 
 public class MyfeatureBranch {
 	 public static void main(String[] args) {
-		System.out.println("MyfeatureBranch");
+		System.out.println("MyfeatureBranch ");
+		System.out.println("adde new line");
 	}
 
 }
